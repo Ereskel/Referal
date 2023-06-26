3f703fce93840a6ddde2f7b4736cff9e8c2e048e
Salix <74873601+Ereskel@users.noreply.github.com>
2023-06-26 06:37:18 -0700

    Update db.py

--- db.py
+++ db.py
@@ -5,12 +5,12 @@ import aiosqlite
 from config import *
 
 
-### ---___--- ЮЗЕР КЛАССЫ ---___--- ###
+# Пользовательские классы
 class withdrawal_of_balance(StatesGroup):
     withdrawal_amount_state = State()
 
 
-### ---___--- АДМИН КЛАССЫ ---___--- ###
+# Админ классы
 class receiving_a_message(StatesGroup):
     receiving_message_state = State()
 
@@ -25,7 +25,7 @@ class changing_the_balance(StatesGroup):
     change_amount_state = State()
 
 
-### ---___--- СОЗДАНИЕ БАЗЫ ДАННЫХ ---___--- ###
+# Создание базы данных
 async def create_database():
     async with aiosqlite.connect("database2.db") as db2:
         await db2.execute("""CREATE TABLE IF NOT EXISTS database(
@@ -40,19 +40,22 @@ async def create_database():
         await db2.commit()
 
 
+# Изменение номера карты 
 async def add_karta(user_id, karta):
     async with aiosqlite.connect('database2.db') as db2:
         await db2.execute("UPDATE database SET karta = ? WHERE user_id = ?", (karta, user_id))
         await db2.commit()
 
 
+# Получение номера карты по user_id
 async def get_kard_number(user_id):
     async with aiosqlite.connect('database2.db') as db2:
         async with db2.execute('SELECT karta FROM database WHERE user_id = ?', (user_id,)) as cursor:
             kard_number = await cursor.fetchall()
             return kard_number
 
-### ---___--- ДОБАВЛЕНИЕ ДАННЫХ В БАЗУ ДАННЫХ ---___--- ###
+
+# Добавление данных
 async def adding_data(user_id, user_username, referrer, referrer_bonus, entry_date):
     async with aiosqlite.connect("database2.db") as db2:
         await db2.execute(
@@ -61,7 +64,7 @@ async def adding_data(user_id, user_username, referrer, referrer_bonus, entry_da
         await db2.commit()
 
 
-### ---___--- ВЫБОР ЧЕГО-ТО ИЗ БАЗЫ ДАННЫХ ---___--- ###
+# Выбор всех пользователей бд
 async def select_all_user_id():
     async with aiosqlite.connect("database2.db") as db2:
         async with db2.execute("SELECT user_id FROM database") as cursor:
@@ -69,6 +72,7 @@ async def select_all_user_id():
             return all_users
 
 
+# Выбор определенной записи по user_id
 async def select_user_id_where_user_id(user_id):
     async with aiosqlite.connect("database2.db") as db2:
         async with db2.execute("SELECT user_id FROM database WHERE user_id = ?", (user_id,)) as cursor:
@@ -76,6 +80,7 @@ async def select_user_id_where_user_id(user_id):
             return user_id
 
 
+# Выбор определенного значения столбца
 async def select_user_username_where_user_name(user_username):
     async with aiosqlite.connect("database2.db") as db2:
         async with db2.execute("SELECT user_username FROM database WHERE user_username = ?",
@@ -84,6 +89,7 @@ async def select_user_username_where_user_name(user_username):
             return user_username[0]
 
 
+# Выбор определенного значения столбца
 async def select_user_id_where_user_username(user_username):
     async with aiosqlite.connect("database2.db") as db2:
         async with db2.execute("SELECT user_id FROM database WHERE user_username = ?", (user_username,)) as cursor:
@@ -91,6 +97,7 @@ async def select_user_id_where_user_username(user_username):
             return user_id[0]
 
 
+# Выбор определенного значения столбца
 async def select_entry_date_where_user_id(user_id):
     async with aiosqlite.connect("database2.db") as db2:
         async with db2.execute("SELECT entry_date FROM database WHERE user_id = ?", (user_id,)) as cursor:
@@ -98,6 +105,7 @@ async def select_entry_date_where_user_id(user_id):
             return entry_date[0]
 
 
+# Выбор определенного значения столбца
 async def select_user_balance_where_user_id(user_id):
     async with aiosqlite.connect("database2.db") as db2:
         async with db2.execute("SELECT user_balance FROM database WHERE user_id = ?", (user_id,)) as cursor:
@@ -105,6 +113,8 @@ async def select_user_balance_where_user_id(user_id):
             return user_balance[0]
 
 
+
+# Выбор определенного значения столбца
 async def select_referrer_id_where_user_id(user_id):
     async with aiosqlite.connect("database2.db") as db2:
         async with db2.execute("SELECT referrer_id FROM database WHERE user_id = ?", (user_id,)) as cursor:
@@ -112,13 +122,14 @@ async def select_referrer_id_where_user_id(user_id):
             return referral[0]
 
 
+# Выбор определенного значения столбца
 async def select_referrer_bonus_where_user_id(user_id):
     async with aiosqlite.connect("database2.db") as db2:
         async with db2.execute("SELECT referrer_bonus FROM database WHERE user_id = ?", (user_id,)) as cursor:
             referrer_bonus = await cursor.fetchone()
             return referrer_bonus[0]
 
-
+# Вывод общего количества рефералов
 async def select_user_id_where_referrer_id_and_referrer_bonus_1(user_id):
     async with aiosqlite.connect("database2.db") as db2:
         async with db2.execute("SELECT user_id FROM database WHERE referrer_id = ? AND referrer_bonus = 1",
@@ -127,37 +138,30 @@ async def select_user_id_where_referrer_id_and_referrer_bonus_1(user_id):
             return number_referrals
 
 
-### ---___--- ИЗМЕНЕНИЕ ЧЕГО-ТО ИЗ БАЗЫ ДАННЫХ ---___--- ###
+# Изменение user_name в таблице
 async def changing_username_where_user_id(user_username, user_id):
     async with aiosqlite.connect("database2.db") as db2:
         await db2.execute("UPDATE database SET user_username = ? WHERE user_id = ?", (user_username, user_id))
         await db2.commit()
 
 
+# Изменение суммы начисления баланса за одного реферала
 async def changing_referrer_bonus_where_user_id(referrer_bonus, user_id):
     async with aiosqlite.connect("database2.db") as db2:
         await db2.execute("UPDATE database SET referrer_bonus = ? WHERE user_id = ?", (referrer_bonus, user_id))
         await db2.commit()
 
 
+# Изменение баланса пользователя
 async def changing_user_balance_where_user_id(change_amount, username_or_id):
     async with aiosqlite.connect("database2.db") as db2:
         await db2.execute("UPDATE database SET user_balance = ? WHERE user_id = ?", (change_amount, username_or_id))
         await db2.commit()
 
 
-async def changing_user_balance_where_user_username(change_amount, username_or_id):
-    async with aiosqlite.connect("database2.db") as db2:
-        await db2.execute("UPDATE database SET user_balance = ? WHERE user_username = ?",
-                         (change_amount, username_or_id))
-        await db2.commit()
-
-
+# Добавление баланса пользователю
 async def adding_user_balance_where_user_id(referral):
     async with aiosqlite.connect("database2.db") as db2:
         await db2.execute("UPDATE database SET user_balance = user_balance + ? WHERE user_id = ?",
                          (amount_per_one, referral,))
         await db2.commit()
-
-
-# ------------------------------------------------------------------------------------------------------------------------------------------------
