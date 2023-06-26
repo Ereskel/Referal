fe216f5ff86bddf60c8ba38c12e0e0c01cf56d5d
Salix <74873601+Ereskel@users.noreply.github.com>
2023-06-26 05:23:23 -0700

    Update config.py

--- config.py
+++ config.py
@@ -15,12 +15,6 @@ admins_id = 1605439713
 amount_per_one = 0.05
 minimal_vivod = 3
 usd_or_rub = 'rub'
+# На выбор несколько валют
 currency = 'USDT'
-#channel = env.str('channel')
-#admin_user = env.str('admin_user')
-#admins_id = env.list('admins_id')
-#amount_per_one = env.str('amount_per_one')
-#minimal_vivod = env.str('minimal_vivod')
-#usd_or_rub = env.str('usd_or_rub')
-#currency = env.str('currency')
 
